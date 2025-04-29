dc90826fcde87ac17d3994ec2888977982fa019e
dart94 <167586944+dart94@users.noreply.github.com>
2025-04-28 17:41:11 -0700

    feat: Enhance Sidebar component with improved layout and styling

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -10,11 +10,11 @@ import icon from "../img/icon.png";
 
 const Sidebar: React.FC = () => {
   return (
-    <div className={styles.sidebar}>
-      <div className={styles.logoContainer}>
-        <img src={icon} alt="icon" />
+    <div className={`${styles.sidebar} w-48 p-2 flex flex-col items-center justify-center relative z-10`}>
+      <div className={`${styles.logoContainer} flex justify-center items-center mb-4`}>
+        <img src={icon} alt="icon" className="w-[90%] h-auto rounded-lg" />
       </div>
-      <nav className={styles.nav}>
+      <nav className={`${styles.nav} mt-4 relative z-10`}>
         <NavigationItem label="Monitor" route="/monitor" icon={<CiMonitor size={20} />} />
         <NavigationItem label="Mensaje" route="/mesaje" icon={<FontAwesomeIcon icon={faWhatsapp} />} />
         <NavigationItem label="Admin" route="/admin" icon={<MdAdminPanelSettings size={20} />} />
