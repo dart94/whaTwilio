ece6184ca17e549c895023519fee76eecfa97382
dart94 <167586944+dart94@users.noreply.github.com>
2025-05-03 11:02:38 -0700

    feat: Update configuration files; add dotenv integration for environment variables and include railway.json

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,8 +1,11 @@
 import mysql from 'mysql2/promise';
+import * as dotenv from 'dotenv';
+dotenv.config(); 
 
 export const connection = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
+  port: Number(process.env.DB_PORT) || 3306,
   password: process.env.DB_PASSWORD || '1234',
   database: process.env.DB_NAME || 'railway2',
 });
