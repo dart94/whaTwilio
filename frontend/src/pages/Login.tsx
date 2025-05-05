264170caddef05967e1dde8cc308fb1d34e322f9
dart94 <167586944+dart94@users.noreply.github.com>
2025-05-05 13:18:05 -0700

    Refactor code structure for improved readability and maintainability

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 // src/pages/Login.tsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import icon from '../img/logo2.png';
+import icon from '../img/AUTO INSIGHTS-04.jpg';
 import { login } from '../services/auth';
 import styles from '../styles/Login.module.css';
 import { toast } from 'react-toastify';
